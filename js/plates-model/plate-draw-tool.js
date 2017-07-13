f2cd09a3d3e0976f798a1003fbb8e5d5ed00223b
pjanik <janikpiotrek@gmail.com>
2017-07-13 16:31:12 +0200

    Limit continent size / plate size ratio to 70%
    [#148359961]

--- js/plates-model/plate-draw-tool.js
+++ js/plates-model/plate-draw-tool.js
@@ -1,3 +1,5 @@
+const MAX_CONTINENTAL_CRUST_RATIO = 0.7
+
 export default function plateDrawTool (plate, fieldId, type) {
   const queue = []
   const visited = {}
@@ -6,8 +8,21 @@ export default function plateDrawTool (plate, fieldId, type) {
   distance[fieldId] = 0
   visited[fieldId] = true
 
+  const plateSize = plate.fields.size
+  let continentSize = 0
+  if (type === 'continent') {
+    plate.fields.forEach(field => {
+      if (field.isContinent) {
+        continentSize += 1
+      }
+    })
+  }
+
   while (queue.length > 0) {
     const field = queue.shift()
+    if (type === 'continent' && field.isOcean && (continentSize + 1) / plateSize > MAX_CONTINENTAL_CRUST_RATIO) {
+      return
+    }
     field.type = type
     field.baseElevation = type === 'ocean' ? 0.25 : 0.6 + Math.random() * 0.1
 
