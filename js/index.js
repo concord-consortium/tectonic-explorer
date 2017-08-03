b65bcfbc3f24653e3bc66217a0d29b202f602270
pjanik <janikpiotrek@gmail.com>
2017-08-03 20:17:25 +0200

    Add Rollbar support
    [#144613049]

--- js/index.js
+++ js/index.js
@@ -3,6 +3,9 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './components/app'
 import * as THREE from 'three'
+import initRollbar from './init-rollbar'
+
+initRollbar()
 
 render(<App />, document.getElementById('app'))
 // Useful for debugging, e.g. it's possible to create new THREE.Vector3 instances that are used internally.
---
+++ js/init-rollbar.js
@@ -0,0 +1,40 @@
+import rollbar from 'rollbar-browser'
+
+export default function initRollbar () {
+  const host = window.location.hostname
+  const pathname = window.location.pathname
+  const branch = pathname.match(/branch\/(.*)\//)
+  const version = pathname.match(/version\/(.*)\//)
+  // For localhost 127.0.0.* 168.192.* and *.local see http://regexr.com/3dl1e
+  const local = host.match(/\blocalhost|\b127\.0\.0|\b192\.168|0\.0\.0\.0|\.local\b/)
+
+  // Guess environment based on the URL.
+  // Note that we use S3 deployment that follows some patterns:
+  // - production code is deployed to the top-level dir
+  // - branches are deployed to /branch/<branch-name> dir
+  // - versions are deployed to /version/<version-num> dir
+  let env = 'production'
+  if (local) {
+    env = 'local';
+  } else if (branch) {
+    env = `branch-${branch[1]}`
+  } else if (version) {
+    env = `version-${version[1]}`
+  }
+
+  if (env === 'local') {
+    // Do not enable rollbar for localhost.
+    return
+  }
+
+  const config = {
+    accessToken: 'c30b3ce072cd4c709c9a0e2ee14d6c57',
+    captureUncaught: true,
+    payload: {
+      environment: env,
+      host: window.location.host
+    }
+  }
+
+  rollbar.init(config)
+}
