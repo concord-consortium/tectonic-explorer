40f771e7b25499dbe46a810da409309f7d852b9f
Eli Kosminsky <ekosminsky@concord.org>
2017-11-28 19:02:48 -0800

    Leaving a border of lower elevation around drawn continents

--- js/plates-model/plate-draw-tool.js
+++ js/plates-model/plate-draw-tool.js
@@ -1,5 +1,17 @@
 const MAX_CONTINENTAL_CRUST_RATIO = 0.5
 
+function drawField (field, type, plateSize, continentSize, elevationModifier = 1) {
+  if (type === 'continent' && field.isOcean && (continentSize + 1) / plateSize > MAX_CONTINENTAL_CRUST_RATIO) {
+    return
+  }
+  field.type = type
+  field.setDefaultProps()
+  field.baseElevation *= elevationModifier
+  if (type === 'continent') {
+    field.baseElevation += 0.1 * Math.random()
+  }
+}
+
 export default function plateDrawTool (plate, fieldId, type) {
   const queue = []
   const visited = {}
@@ -20,21 +32,20 @@ export default function plateDrawTool (plate, fieldId, type) {
 
   while (queue.length > 0) {
     const field = queue.shift()
-    if (type === 'continent' && field.isOcean && (continentSize + 1) / plateSize > MAX_CONTINENTAL_CRUST_RATIO) {
-      return
-    }
-    field.type = type
-    field.setDefaultProps()
-    if (type === 'continent') {
-      field.baseElevation += 0.1 * Math.random()
-    }
+    drawField(field, type, plateSize, continentSize)
 
     const newDistance = distance[field.id] + Math.random() * 5
     field.forEachNeighbour(otherField => {
-      if (!visited[otherField.id] && newDistance < 7) {
-        visited[otherField.id] = true
-        distance[otherField.id] = newDistance
-        queue.push(otherField)
+      if (!visited[otherField.id]) {
+        if (newDistance < 7) {
+          visited[otherField.id] = true
+          distance[otherField.id] = newDistance
+          queue.push(otherField)
+        } else {
+          if (type === 'continent' && otherField.type === 'ocean') {
+            drawField(otherField, 'continent', plateSize, continentSize + 1, .7)
+          }
+        }
       }
       if (visited[otherField.id] && newDistance < distance[otherField.id]) {
         distance[otherField.id] = newDistance
