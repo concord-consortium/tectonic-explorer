3b89d77eb0b93aadc1358134dcdbc21dae724905
pjanik <janikpiotrek@gmail.com>
2018-01-15 17:52:59 -0800

    Make part of the About dialog conditional (only when planet wizard is enabled)
    [#154259749]

--- js/components/about-dialog-content.js
+++ js/components/about-dialog-content.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react'
+import config from '../config'
 
 export default class AboutDialogContent extends PureComponent {
   render () {
@@ -8,10 +9,13 @@ export default class AboutDialogContent extends PureComponent {
           Geologists use models to explore the mechanisms and physical processes that shape Earth’s surface.
           How can this model help explain past and current movements tectonic plates are responsible for most surface features and events on Earth?
         </p>
-        <p>
-          Set up an Earth-like planet. Click the play button to see how plates interact. Change direction of plate motion,
-          change location of continents, learn how density influences plate interactions.
-        </p>
+        {
+          config.planetWizard &&
+          <p>
+            Set up an Earth-like planet. Click the play button to see how plates interact. Change direction of plate motion,
+            change location of continents, learn how density influences plate interactions.
+          </p>
+        }
         <p>
           Make a cross-section to see a three-dimensional view a region below Earth’s surface. How do interactions at
           the surface of the Earth-like planet reflect what is happening below Earth’s surface?
