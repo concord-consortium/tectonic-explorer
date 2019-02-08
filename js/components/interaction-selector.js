1c75baf7d7efd815d79c98e9f79a25e59773e976
pjanik <janikpiotrek@gmail.com>
2019-02-08 15:29:13 +0100

    Fix typo that broke the app
    [#162763011]

--- js/components/interaction-selector.js
+++ js/components/interaction-selector.js
@@ -28,8 +28,8 @@ export default class InteractionSelector extends PureComponent {
     const activeClass = targetInteraction === interaction ? 'active' : ''
     const handler = () => { setInteraction(targetInteraction) }
     return (
-      <Button key={targetInteraction} className={`large-button ${activeClass}`} 
-      data-test={INTRERACTION_LABELS[targetInteractio]} onClick={handler}>
+      <Button key={targetInteraction} className={`large-button ${activeClass}`}
+      data-test={INTRERACTION_LABELS[targetInteraction]} onClick={handler}>
         <FontIcon value={ICON[targetInteraction]} />
         <div className='label'>{INTRERACTION_LABELS[targetInteraction]}</div>
       </Button>
