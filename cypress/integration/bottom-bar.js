9cdf34511d872324a5ce14e4c8e377d0dcb3f467
pjanik <janikpiotrek@gmail.com>
2019-02-08 15:42:46 +0100

    Add Eli's integration test (@ekosmin)
    [#162763011]

---
+++ cypress/integration/bottom-bar.js
@@ -0,0 +1,49 @@
+let panelHeight = 0
+
+describe('Bottom Bar', function () {
+  beforeEach(() => {
+    cy.visit('http://localhost:8080/?preset=subduction')
+    cy.wait(2000) // wait for splash screen to close
+    if (!panelHeight) {
+      cy.get('.bottom-panel')
+        .then((panel) => {
+          panelHeight = panel[0].clientHeight
+        })
+    }
+  })
+
+  afterEach(() => {
+    cy.get('.bottom-panel')
+      .should('be.visible')
+      .then((panel) => {
+        expect(panel[0].clientHeight).to.eq(panelHeight)
+      })
+  })
+
+  it ('Resizes the logo', function () {
+    cy.get('.cc-logo-large').should('be.visible')
+    cy.get('.cc-logo-small').should('not.be.visible')
+
+    cy.viewport(800, 660)
+    cy.get('.cc-logo-large').should('not.be.visible')
+    cy.get('.cc-logo-small').should('be.visible')
+  })
+
+  it ('Shows and hides the sidebar', function () {
+    cy.get('.sidebar').should('not.be.visible')
+    cy.contains('menu').click()
+    cy.get('.sidebar').should('be.visible')
+    cy.contains('close').click()
+    cy.get('.sidebar').should('not.be.visible')
+  })
+
+  it ('Has functional checkboxes', function () {
+    cy.contains('menu').click()
+
+    cy.contains('Latitude and longitude lines').click()
+    cy.contains('Force arrows').click()
+    cy.contains('Euler poles').click()
+    cy.contains('Plate boundaries').click()
+    cy.contains('Wireframe').click()
+  })
+})
