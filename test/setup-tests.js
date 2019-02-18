bd1b3a67b671b824104f2b05cfd88e57990e2efe
pjanik <janikpiotrek@gmail.com>
2019-02-18 17:14:33 +0100

    Add generic tests related to sidebar menu (that cover Earthquake checkbox too)
    [#163767335]

--- test/setup-tests.js
+++ test/setup-tests.js
@@ -1,5 +1,17 @@
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-15'
+import config from '../js/config'
 
 // see http://airbnb.io/enzyme/docs/installation/index.html
 configure({ adapter: new Adapter() })
+
+const originalConfig = Object.assign({}, config)
+global.resetConfig = () => {
+  // Restore config as it can be modified by tests.
+  Object.assign(config, originalConfig)
+  Object.keys(config).forEach(key => {
+    if (originalConfig[key] === undefined) {
+      delete config[key]
+    }
+  })
+}
