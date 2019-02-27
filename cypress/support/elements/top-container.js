2576e1d958c89b2b6a6964bf3618e513eb8e517b
Kevin Santos <ksantos@CC-KSANTOS.local>
2019-02-27 13:50:53 -0500

    Updates to smoke test and fixing lint errors

--- cypress/support/elements/top-container.js
+++ cypress/support/elements/top-container.js
@@ -1,30 +1,7 @@
 class TopContainer {
-  getDrawContinents () {
-    return cy.get('[data-test=draw-continents')
-    // return cy.get('.interaction-selector > .large-button > .label').contains('Draw Continents');
-  }
-
-  getEraseContinents () {
-    return cy.get('[data-test=erase-continents')
-    // return cy.get('.interaction-selector > .large-button > .label').contains('Erase Continents');
-  }
-
-  getDrawCrossSection () {
-    return cy.get('[data-test=draw-cross-section')
-    // return cy.get('.interaction-selector > .large-button > .label').contains('Draw cross section');
-  }
-
-  getDrawForceVector () {
-    return cy.get('[data-test=draw-force-vectors')
-    // return cy.get('.interaction-selector > .large-button > .label').contains('Draw force vectors');
-  }
-
-  getRotateCamera() {
-    return cy.get('[data-test=rotate-camera')
-    // return cy.get('.interaction-selector > .large-button > .label').contains('Rotate Camera');
-  }
-  getInteractionSelector(option) {
-    return cy.contains(option)
+  getInteractionSelector (option) {
+    let selector = option
+    return cy.contains(selector)
   }
 
   getRefresh () {
@@ -39,8 +16,15 @@ class TopContainer {
     return cy.get('.top-bar--about--pqGn8uTf')
   }
 
+  getRotateCamera() {
+    return cy.get('[data-test=rotate-camera')
+    // return cy.get('.interaction-selector > .large-button > .label').contains('Rotate Camera');
+  }
   getResetCameraOrientation () {
     return cy.get('.planet-view > .camera-reset')
+  }
+  getInteractionSelector(option) {
+    return cy.contains(option)
   }
 }
 export default TopContainer
