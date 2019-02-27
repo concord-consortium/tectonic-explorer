f93e39c91a7d29cdf1e42b2ae60d697dadf82241
Kevin Santos <ksantos@CC-KSANTOS.local>
2019-02-27 10:56:22 -0500

    Add smoke test and update canvas

--- cypress/support/elements/planet-wizard.js
+++ cypress/support/elements/planet-wizard.js
@@ -1,36 +1,48 @@
 class PlanetWizard {
-  getPlateNumOptions (num) {
+  getAllPlateNumOptions () {
+    return cy.get('[data-test=plate-num-options]')
+  }
+
+  getPlateNumOption (num) {
     switch (num) {
       case ('2'):
-        return cy.get('[data-test=interaction-selector]').eq(0)
+        return cy.get('[data-test=plate-num-options] > :nth-child(1)')
       case ('3'):
-        return cy.get('[data-test=interaction-selector]').eq(1)
+        return cy.get('[data-test=plate-num-options] > :nth-child(2)')
       case ('4'):
-        return cy.get('[data-test=interaction-selector]').eq(2)
+        return cy.get('[data-test=plate-num-options] > :nth-child(3)')
       case ('5'):
-        return cy.get('[data-test=interaction-selector]').eq(3)
-      case ('5-UD'):
-        return cy.get('[data-test=interaction-selector]').eq(4)
+        return cy.get('[data-test=plate-num-options] > :nth-child(4)')
+      case ('5UD'):
+        return cy.get('[data-test=plate-num-options] > :nth-child(5)')
     }
+  }
+
+  getAllPlanetDensityOptions () {
+    return cy.get('.densities').find('li')
   }
 
   getPlanetDensityOptions (num) {
     switch (num) {
       case ('1'):
-        return cy.get('')
+        return cy.get('.densities').find('li').eq(0)
       case ('2'):
-        return cy.get('')
+        return cy.get('.densities').find('li').eq(1)
       case ('3'):
-        return cy.get('')
+        return cy.get('.densities').find('li').eq(2)
       case ('4'):
-        return cy.get('')
-      case ('5'):
-        return cy.get('')
+        return cy.get('.densities').find('li').eq(3)
+      case ('5UD'):
+        return cy.get('.densities').find('li').eq(4)
     }
   }
 
   getColorKey () {
-    cy.get('[data-test=color-key]')
+    return cy.get('[data-test=color-key]')
+  }
+
+  getTimeDisplay () {
+    return cy.get('[data-test=time-display]')
   }
 }
 export default PlanetWizard
--- cypress/support/elements/top-container.js
+++ cypress/support/elements/top-container.js
@@ -19,9 +19,29 @@ class TopContainer {
     // return cy.get('.interaction-selector > .large-button > .label').contains('Draw force vectors');
   }
 
-  getRotateCamera () {
+  getRotateCamera() {
     return cy.get('[data-test=rotate-camera')
     // return cy.get('.interaction-selector > .large-button > .label').contains('Rotate Camera');
+  }
+  getInteractionSelector(option) {
+    let selector = option;
+    return cy.contains(option)
+  }
+
+  getRefresh () {
+    return cy.get('.top-bar--topBar--lXu1iRHL > .material-icons')
+  }
+
+  getShare () {
+    return cy.get('.top-bar--share--181dZdzQ')
+  }
+
+  getAbout () {
+    return cy.get('.top-bar--about--pqGn8uTf')
+  }
+
+  getResetCameraOrientation () {
+    return cy.get('.planet-view > .camera-reset')
   }
 }
 export default TopContainer
