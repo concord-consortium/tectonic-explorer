899e200429e0b49c65bda648a11928a122cea509
pjanik <janikpiotrek@gmail.com>
2021-02-17 14:52:37 +0100

    Update Cypress version, tests and snapshots
    [#176905512]
    [#176905549]

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,14 +1,13 @@
 const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin')
 
 module.exports = (on, config) => {
-  on('before:browser:launch', browser => {
+  on('before:browser:launch', (browser, launchOptions)  => {
     // Note that it needs to match or exceed viewportHeight and viewportWidth values specified in cypress.json.
     if (browser.name === 'electron') {
-      return {
-        width: 1400,
-        height: 1000
-      }
+      launchOptions.preferences.width = 1400
+      launchOptions.preferences.height = 1000
     }
+    return launchOptions;
   })
 
   addMatchImageSnapshotPlugin(on, config)
