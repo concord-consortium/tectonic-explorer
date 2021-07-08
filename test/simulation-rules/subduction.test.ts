bb04a295a9bc495509f3488a5fce3d47992eb335
pjanik <janikpiotrek@gmail.com>
2021-07-08 14:24:34 +0200

    Update serialization and subduction preset tests
    [#178131594]

--- test/simulation-rules/subduction.test.ts
+++ test/simulation-rules/subduction.test.ts
@@ -18,10 +18,8 @@ test("subudction model is loaded correctly", () => {
   const model = new Model(modelImgData, presets.subduction.init!);
   expect(model.plates.length).toEqual(2);
 
-  console.log(model.getPlate(0)?.fields.get(1318)?.crustThickness);
-  console.log(model.getPlate(0)?.fields.get(1318)?.crust.rockLayers);
-
-  // expect(model.getPlate(0)?.fields.get(1318)?.elevation).toBeCloseTo(0.025);
-  // expect(model.getPlate(1)?.fields.get(1066)?.elevation).toBeCloseTo(0.3660);
-  // expect(model.getPlate(1)?.fields.get(821)?.elevation).toBeCloseTo(0.5490);
+  // These values have been obtained manually in the web browser.
+  expect(model.getPlate(0)?.fields.get(1318)?.elevation).toBeCloseTo(0.025);
+  expect(model.getPlate(1)?.fields.get(1066)?.elevation).toBeCloseTo(0.3660);
+  expect(model.getPlate(1)?.fields.get(821)?.elevation).toBeCloseTo(0.5490);
 });
