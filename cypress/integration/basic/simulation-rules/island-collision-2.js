e2d5d5866972a89e6a802f143d24b28b8f5a5fb8
pjanik <janikpiotrek@gmail.com>
2021-07-09 15:24:49 +0200

    Move simulation-based tests to Cypress, add more examples
    [#178131594]
    
    Better peformance and should be easier to add new ones for QA.

---
+++ cypress/integration/basic/simulation-rules/island-collision-2.js
@@ -0,0 +1,38 @@
+import Model from "../../../../js/plates-model/model";
+import presets from "../../../../js/presets";
+import { getFieldElevation, getFieldRockLayers, runModelFor } from "../../../../js/plates-model/model-test-helpers";
+import { getImageData } from "../../../../js/utils";
+
+let modelImgData = null;
+
+before(done => {
+  getImageData("/" + presets.islandCollision2.img, (imgData) => {
+    modelImgData = imgData;
+    done();
+  });
+});
+
+const delta = 0.01;
+
+describe("Island Collision 2 model", () => {
+  it("loads correctly and after some time the small islands are scraped and added to the top plate", () => {
+    const model = new Model(modelImgData, presets.islandCollision2.init);
+    
+    // Left plate - island.
+    expect(getFieldElevation(model, 0, 2428)).to.be.closeTo(0.67, delta);
+    // Right plate - continental shelf.
+    expect(getFieldElevation(model, 1, 998)).to.be.closeTo(0.33, delta);
+
+    runModelFor(model, 100); // million years
+
+    // Right plate - continental shelf with islands leftovers (oceanic volcanic rocks).
+    expect(getFieldElevation(model, 1, 998)).to.be.greaterThan(0.85);
+    const rockLayers = getFieldRockLayers(model, 1, 998);
+    expect(rockLayers?.[0].rock).to.equal("Oceanic Sediment");
+    expect(rockLayers?.[1].rock).to.equal("Andesite");
+    expect(rockLayers?.[2].rock).to.equal("Diorite");
+
+    // Left plate - island scraped and subducting.
+    expect(getFieldElevation(model, 0, 2428)).to.be.lessThan(0);
+  });
+});
