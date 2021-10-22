fc19e8bc382bff5424faab72446b65ffcf8f5124
pjanik <janikpiotrek@gmail.com>
2021-10-22 19:04:19 +0200

    Fix ColorKey tests
    [#177652612]

--- test/components/color-key.test.tsx
+++ test/components/color-key.test.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Provider } from "mobx-react";
 import { mount } from "enzyme";
 import ColorKey from "../../js/components/color-key";
 
@@ -19,30 +20,50 @@ describe("Color Key component", () => {
   });
 
   it("displays by default", () => {
-    const wrapper = mount(<ColorKey simulationStore={store} />);
+    const wrapper = mount(
+      <Provider simulationStore={store}>
+        <ColorKey />
+      </Provider>
+    );
     expect(wrapper.html()).toEqual(expect.stringContaining('data-test="color-key-plates"'));
   });
   it("can toggle the key", () => {
-    const wrapper = mount(<ColorKey simulationStore={store} />);
+    const wrapper = mount(
+      <Provider simulationStore={store}>
+        <ColorKey />
+      </Provider>
+    );
     wrapper.find('[data-test="key-close-button"]').at(0).simulate("click");
     expect(store.setOption).toHaveBeenLastCalledWith("key", false);
   });
   it("can display earthquake key", () => {
     store.key = true;
     store.earthquakes = true;
-    const wrapper = mount(<ColorKey simulationStore={store} />);
+    const wrapper = mount(
+      <Provider simulationStore={store}>
+        <ColorKey />
+      </Provider>
+    );
     expect(wrapper.html()).toEqual(expect.stringContaining('data-test="color-key-earthquakes"'));
   });
   it("can display volcano key", () => {
     store.key = true;
     store.volcanicEruptions = true;
-    const wrapper = mount(<ColorKey simulationStore={store} />);
+    const wrapper = mount(
+      <Provider simulationStore={store}>
+        <ColorKey />
+      </Provider>
+    );
     expect(wrapper.html()).toEqual(expect.stringContaining('data-test="color-key-volcanic-eruptions"'));
   });
   it("can display cross-section key", () => {
     store.key = true;
     store.crossSectionVisible = true;
-    const wrapper = mount(<ColorKey simulationStore={store} />);
+    const wrapper = mount(
+      <Provider simulationStore={store}>
+        <ColorKey />
+      </Provider>
+    );
     expect(wrapper.html()).toEqual(expect.stringContaining('data-test="color-key"'));
   });
 });
