199e8efaaf1259dc47d867217b8e7698693ee97d
pjanik <janikpiotrek@gmail.com>
2021-10-25 19:05:34 +0200

    Remove 2D cross-section
    [#180051337]
    
    It's not used, so it doesn't make sense to maintain it (and implement event handlers for it).

--- js/components/cross-section.tsx
+++ js/components/cross-section.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import { inject, observer } from "mobx-react";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 import SmallButton from "./small-button";
-import CrossSection2D from "./cross-section-2d";
 import CrossSection3D from "./cross-section-3d";
-import config from "../config";
 import { BaseComponent, IBaseProps } from "./base";
 
 import "../../css/cross-section.less";
@@ -25,7 +23,7 @@ export default class CrossSection extends BaseComponent<IBaseProps, IState> {
             <CSSTransition classNames="slide" timeout={{ exit: CROSS_SECTION_TRANSITION_LENGTH, enter: CROSS_SECTION_TRANSITION_LENGTH }}>
               <div key="cross-section" className="cross-section-content">
                 <div className="container">
-                  { config.crossSection3d ? <CrossSection3D /> : <CrossSection2D /> }
+                  <CrossSection3D />
                   <SmallButton className="close-button" icon="close" onClick={closeCrossSection} data-test="cross-section-close">
                     Close Cross-section
                   </SmallButton>
