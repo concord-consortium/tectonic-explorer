82f63e1a8b902a6491156a1eed3889a1eb13107a
pjanik <janikpiotrek@gmail.com>
2021-11-16 17:17:51 +0100

    Update Cypress tests
    [#179940030]
    [#180267346]

--- cypress/integration/basic/bottom-bar.js
+++ cypress/integration/basic/bottom-bar.js
@@ -12,12 +12,4 @@ describe("Bottom Bar", function() {
     cy.get(".cc-logo-large").should("not.be.visible");
     cy.get(".cc-logo-small").should("be.visible");
   });
-
-  it("Shows and hides the sidebar", function() {
-    cy.get(".sidebar-menu--sidebar--tectonic-explorer").should("not.exist");
-    cy.contains("menu").click();
-    cy.get(".sidebar-menu--sidebar--tectonic-explorer").should("be.visible");
-    cy.contains("close").click();
-    cy.get(".sidebar-menu--sidebar--tectonic-explorer").should("not.exist");
-  });
 });
