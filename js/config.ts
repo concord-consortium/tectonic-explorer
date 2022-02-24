fd3e7538c854486519187fba1d0e4e15557f27c1
pjanik <janikpiotrek@gmail.com>
2022-02-24 14:31:42 -0800

    Update continentalStretchingRatio to fit better new force values
    [#181153609]

--- js/config.ts
+++ js/config.ts
@@ -85,7 +85,7 @@ const DEFAULT_CONFIG = {
   oceanDensity: 1,
   continentDensity: 3,
   // How fast continent is stretching along divergent boundary. Bigger value means it would turn into ocean / see faster.
-  continentalStretchingRatio: 8,
+  continentalStretchingRatio: 4,
   // Max length of the cross-section line
   maxCrossSectionLength: 4000, // km
   // Horizontal scaling of cross-section data.
