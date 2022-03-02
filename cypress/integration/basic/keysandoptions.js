bfa9805f2bd0559c77d30df50b4048c2523644ff
pjanik <janikpiotrek@gmail.com>
2022-03-01 21:22:58 -0800

    Key: Rock Types -> Rock Type
    [#181127407]

--- cypress/integration/basic/keysandoptions.js
+++ cypress/integration/basic/keysandoptions.js
@@ -25,7 +25,7 @@ describe("Keys And Options", function() {
   it("Map Type Tab", function() {
     BottomContainer.getTakeSample().click();
     KeyAndOptions.getMapTypeKey().should("be.visible").should("contain", "Key: Elevation");
-    KeyAndOptions.getKeyRockType().should("be.visible").get(".rock-types--title--tectonic-explorer").should("contain", "Key: Rock Types");
+    KeyAndOptions.getKeyRockType().should("be.visible").get(".rock-types--title--tectonic-explorer").should("contain", "Key: Rock Type");
 
     //verfiy different rock types displayed
     KeyAndOptions.getRockKeyNumOption("1").get(".rock-types--headerLabel--tectonic-explorer").contains("Igneous Rocks");
