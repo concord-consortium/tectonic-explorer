e9fd3ec9de6fc9cdeff4dfe4f03906fe7c0f67e1
pjanik <janikpiotrek@gmail.com>
2022-05-17 16:52:16 +0200

    Remove alternative plate2/3 colors from plate color key
    [#182121571]

--- js/components/keys/plate-color-key.tsx
+++ js/components/keys/plate-color-key.tsx
@@ -6,27 +6,22 @@ import { plateColorKeyHeight } from "./key-types";
 import ModelStore from "../../stores/model-store";
 import Plate1Color from "../../../images/plate-1-color.svg";
 import Plate2Color from "../../../images/plate-2-color.svg";
-import Plate2ColorAlt from "../../../images/plate-2-color-alt.svg";
 import Plate3Color from "../../../images/plate-3-color.svg";
-import Plate3ColorAlt from "../../../images/plate-3-color-alt.svg";
 import Plate4Color from "../../../images/plate-4-color.svg";
 import Plate5Color from "../../../images/plate-5-color.svg";
 
 import css from "../../../css-modules/keys/map-type.less";
 
-const plateCountToPlateColors: any[][] = [
-  [Plate1Color],
-  [Plate1Color, Plate2Color],
-  [Plate1Color, Plate2ColorAlt, Plate3ColorAlt],
-  [Plate1Color, Plate2Color, Plate3Color, Plate4Color],
-  [Plate1Color, Plate2Color, Plate3Color, Plate4Color, Plate5Color],
+const plateColorGradients: any[] = [
+  Plate1Color, Plate2Color, Plate3Color, Plate4Color, Plate5Color
 ];
+
 interface IProps {
   model: ModelStore;
 }
 
 export const PlateColorKey = observer(({ model }: IProps) => {
-  const plateColors = plateCountToPlateColors[model.plates.length - 1];
+  const plateColors = plateColorGradients.slice(0, model.plates.length);
   return (
     <div className={`${css.mapType} ${css.plateColorKey}`} data-test="map-type-key">
       <div className={css.plateColorTitle}>Key: Plate Color (with elevation)</div>
