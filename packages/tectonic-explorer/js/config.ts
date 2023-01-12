186080f5017347f823c74e875cc809238a9871d0
pjanik <janikpiotrek@gmail.com>
2023-01-12 18:49:57 +0100

    Add "id" column to default value of "dataSampleColumns"

--- packages/tectonic-explorer/js/config.ts
+++ packages/tectonic-explorer/js/config.ts
@@ -32,7 +32,7 @@ const DEFAULT_CONFIG = {
   showTakeSampleButton: true,
   // If true, show the Collect Data button in the bottom bar
   showCollectDataButton: false,
-  dataSampleColumns: ["category", "type", "temperatureAndPressure"] as DataSampleColumnName[],
+  dataSampleColumns: ["id", "category", "type", "temperatureAndPressure"] as DataSampleColumnName[],
   // If true, show the temperature/pressure tool button in the bottom bar
   showTempPressureTool: true,
   // If true, show the earthquakes switch in the bottom bar
