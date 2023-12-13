27f2221797268e267e802e5a1b556cefeba58b88
433eros <115961721+433eros@users.noreply.github.com>
2023-12-13 09:29:48 -0500

    chore(webpack): Generate index-top.html for tecrock-table

--- packages/tecrock-table/webpack.config.js
+++ packages/tecrock-table/webpack.config.js
@@ -5,10 +5,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const webpackCommon = require('./webpack-common.config.js');
 
+const DEPLOY_PATH = process.env.DEPLOY_PATH;
+
 module.exports = (env, argv) => {
   const interactiveName = path.basename(__dirname); // e.g. "open-response"
 
   return webpackCommon(env, argv, __dirname, {
+    stats: {
+      children: true,
+    },
     // Add custom webpack configuration here
     entry: {
       [`${interactiveName}`]: './src/index.tsx'
@@ -18,7 +23,12 @@ module.exports = (env, argv) => {
         chunks: [interactiveName],
         filename: `${interactiveName}/index.html`,
         template: './index.html'
-      })
+      }),
+      ...(DEPLOY_PATH ? [new HtmlWebpackPlugin({
+        filename: 'tecrock-table/index-top.html',
+        template: './index.html',
+        publicPath: DEPLOY_PATH,
+      })] : [])
     ]
   });
 };
